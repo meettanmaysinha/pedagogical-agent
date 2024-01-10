5dc82c7a4fdf306e862a664ccde3cdbc8767848b
chengyaolee <chengyao.lee@gmail.com>
2024-01-10 18:59:07 +0800

    Added Streamlit Interface for Batch Upload

--- batchuploader.py
+++ batchuploader.py
@@ -7,16 +7,18 @@ from hume.models.config import ProsodyConfig
 from apikey import API_KEY
 
 class FileProcessor:
-    def __init__(self, configs=FaceConfig(), file_path=None):
+    def __init__(self, configs=[FaceConfig(),ProsodyConfig()], file_path=None):
+        '''Initialise Hume Batch API'''
         self.client = HumeBatchClient(API_KEY)
         self.configs = configs
-        self.file_path = file_path
+        self.file_path = file_path if isinstance(file_path, list) else [file_path]
 
     def set_filepath(self,file_path):
+        '''Sets file path of clip being analysed'''
         self.file_path = file_path
 
     def process_file(self):
-        # configs = [FaceConfig(identify_faces=True), ProsodyConfig()]
+        '''Processes file using Hume API'''
         job = self.client.submit_job(None, self.configs, files=self.file_path)
         
         print(job)
@@ -28,8 +30,9 @@ class FileProcessor:
         print("Predictions downloaded to predictions.json")
         return predictions
 
+    # Not implemented yet
     def split_file(self, interval = 10):
-        # Split file into chunks
+        '''Splits large file into chunks for Hume API call'''
         file_size_limit = 100000000 # 100MB
         cmd = f"python ffmpeg-split.py -f big_video_file.mp4 -s {interval} -S {file_size_limit}"
         subprocess.call(cmd, shell=True)
@@ -38,18 +41,24 @@ class EmotionsAnalyser:
         # self.dataframe = None
 
     def display_file_details(self, prediction=None):
+        '''Displays file details, such as type and filename'''
         print(prediction[0]["source"])
     
     def to_dataframe(self, prediction=None):
-        # Extract results from Face model
-        face_dataframe = pd.json_normalize(prediction[0]["results"]["predictions"][0]["models"]["face"]["grouped_predictions"][0]["predictions"])
-        face_dataframe.apply(lambda x: self.sort_emotions(dataframe=x["emotions"]),axis=1)
-        self.extract_top_3_emotions(dataframe=face_dataframe)
+        '''Converts face and prosody results to a dataframe'''
+        face_dataframe, prosody_dataframe = pd.DataFrame(), pd.DataFrame()
+        if "face" in prediction[0]["results"]["predictions"][0]["models"]:
+            # Extract results from Face model
+            face_dataframe = pd.json_normalize(prediction[0]["results"]["predictions"][0]["models"]["face"]["grouped_predictions"][0]["predictions"])
+            face_dataframe.apply(lambda x: self.sort_emotions(dataframe=x["emotions"]),axis=1)
+            self.extract_top_3_emotions(dataframe=face_dataframe)
 
-        # Extract results from Prosody model
-        prosody_dataframe = pd.json_normalize(prediction[0]["results"]["predictions"][0]["models"]["prosody"]["grouped_predictions"][0]["predictions"])
-        prosody_dataframe.apply(lambda x: self.sort_emotions(dataframe=x["emotions"]),axis=1)
-        self.extract_top_3_emotions(dataframe=prosody_dataframe)
+        if "prosody" in prediction[0]["results"]["predictions"][0]["models"]:
+            # Extract results from Prosody model
+            prosody_dataframe = pd.json_normalize(prediction[0]["results"]["predictions"][0]["models"]["prosody"]["grouped_predictions"][0]["predictions"])
+            prosody_dataframe.apply(lambda x: self.sort_emotions(dataframe=x["emotions"]),axis=1)
+            self.extract_top_3_emotions(dataframe=prosody_dataframe)
+        
         return face_dataframe, prosody_dataframe
     
     def sort_emotions(self, dataframe=None,sort_by='score',reverse=True):
@@ -71,25 +80,28 @@ class EmotionsAnalyser:
 
     def most_common_occurence(self, dataframe=None):
         # Get most common emotion
-        most_common_emotion = dataframe['first_emotion'].value_counts().idxmax()
-        most_common_emotion_occurences = dataframe['first_emotion'].value_counts()[most_common_emotion]
-        return most_common_emotion, most_common_emotion_occurences
-    
+        try:
+            most_common_emotion = dataframe['first_emotion'].value_counts().idxmax()
+            most_common_emotion_occurences = dataframe['first_emotion'].value_counts()[most_common_emotion]
+            return most_common_emotion, most_common_emotion_occurences
+        except:
+            return None, None
+        
     def export_results(self, dataframe=None, filename="results.csv"):
         # Export dataframe to csv
         dataframe.to_csv(filename,index=False)
 
-# Example usage
-# FILE_PATH = ["/Users/chengyao/Downloads/Hume_Test_Video_Football.mp3"]
-FILE_PATH = ["/Users/chengyao/Downloads/Test_Hume_Comedian.mp4"]
-configs = [FaceConfig(), ProsodyConfig()]
-processor = FileProcessor(configs,file_path=FILE_PATH)
+# # Example usage
+# # FILE_PATH = ["/Users/chengyao/Downloads/Hume_Test_Video_Football.mp3"]
+# FILE_PATH = ["/Users/chengyao/Downloads/Test_Hume_Comedian.mp4"]
+# configs = [FaceConfig(), ProsodyConfig()]
+# processor = FileProcessor(configs,file_path=FILE_PATH)
 
-predictions = processor.process_file()
-analyser = EmotionsAnalyser()
-analyser.display_file_details(predictions)
-face_df, prosody_df = analyser.to_dataframe(predictions)
-analyser.export_results(face_df,"face_results.csv")
-analyser.export_results(prosody_df,"prosody_results.csv")
-print("The most common facial occurence of emotion is: ", (analyser.most_common_occurence(dataframe=face_df)))
-print("The most common prosody occurence of emotion is: ", (analyser.most_common_occurence(dataframe=prosody_df)))
+# predictions = processor.process_file()
+# analyser = EmotionsAnalyser()
+# analyser.display_file_details(predictions)
+# face_df, prosody_df = analyser.to_dataframe(predictions)
+# analyser.export_results(face_df,"face_results.csv")
+# analyser.export_results(prosody_df,"prosody_results.csv")
+# print("The most common facial occurence of emotion is: ", (analyser.most_common_occurence(dataframe=face_df)))
+# print("The most common prosody occurence of emotion is: ", (analyser.most_common_occurence(dataframe=prosody_df)))

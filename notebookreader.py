58330634f7024c9f2eb9b2ec4a62edeee115ee44
chengyaolee <chengyao.lee@gmail.com>
2024-03-10 20:46:04 +0800

    Convert NB reader as module

--- notebookreader.py
+++
@@ -1,9 +0,0 @@
-import json 
-import pandas as pd
-
-with open("Pipeline Testing.ipynb", mode= "r", encoding= "utf-8") as f:
-    myfile = json.loads(f.read())
-
-# converting json dataset from dictionary to dataframe
-nb_content = pd.json_normalize(myfile["cells"])
-print(nb_content)

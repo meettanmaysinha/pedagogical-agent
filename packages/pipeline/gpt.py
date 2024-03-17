ee00728157d15db558ae9dd8a15da34669c9ad8e
chengyaolee <chengyao.lee@gmail.com>
2024-03-17 23:50:07 +0800

    Formatting and future logit_bias configuration

--- packages/pipeline/gpt.py
+++ packages/pipeline/gpt.py
@@ -1,8 +1,13 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+# import nltk
+# nltk.download('punkt')  # Download the punkt tokenizer models if not already downloaded
+
+# from nltk.tokenize import word_tokenize
 
 load_dotenv()
+
 
 openai = OpenAI(
     api_key = os.getenv('OPENAI_API_KEY')
@@ -13,8 +18,15 @@ def get_chat_response(cells_content, emotions):
         model="gpt-3.5-turbo",
         messages=[
             {"role":"system","content":"You are a programming teacher, skilled in explaining complex programming concepts without divulging answers. Guide the students towards the answer, helping them with errors in their code."},
-            {"role":"user","content":f"I am a student who is facing issues with my code: {cells_content}. For each error/output in 'output', the code snippet is in 'source'. I am feeling {emotions}. Encourage me and help me understand my problems better. Give me the code snippet in 'source' by quoting it"}
-        ]
+            {"role":"user","content": "I am a student who is facing issues with my code." \
+             f"Here is my extracted cell content from Jupyter Notebook: {cells_content}."\
+             "For each error/output in output, the code snippet is in source. " \
+             f"I am feeling {emotions}. Help me understand my problems better." \
+             "Give me the error code snippet in source by quoting it." \
+             "Do not explicitly mention the dataset given to you, but you may mention its values."}
+        ],
+        # logit_bias = {word_tokenize("dataset")[0]: -1,},
+
     )
     # print(completion.choices[0].message)
 
